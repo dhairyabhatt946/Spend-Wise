428435d3a986fe1b0123f8360b9788617823792e
Paresh Bhatt <dhairyabhatt946@gmail.com>
2026-03-19 20:48:47 +0530

    fixed problems in prisma

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,12 +1,26 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "../generated/prisma/client";
 
-const adapter = new PrismaPg({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-});
+// const adapter = new PrismaPg({
+//     host: process.env.DATABASE_HOST,
+//     port: process.env.DATABASE_PORT,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE_NAME,
+// });
 
-export const prisma = new PrismaClient({adapter});
+// export const prisma = new PrismaClient({adapter});
+
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+// Prisma 7 explicitly requires an adapter to connect to the database
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL! 
+})
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
